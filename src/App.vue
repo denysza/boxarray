58a01edcc27f1451501d3e12271dd2c09463dcc0
sa09nen <ryosuke.sakai@enechange.co.jp>
2021-12-23 18:52:55 +0100

    update data structure

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,27 @@
 import BoxArray from './components/BoxArray.vue'
 </script>
 
+
 <template>
-  <BoxArray :array="array" />
+  <BoxArray :array="array" :left-array="leftArray" :right-array="rightArray" />
 </template>
 <script>
-  export default {
-    data: () => ({
-      array:
-       [
-        {"leftID": "left1", "rightID": "right2", "priority": 1},
-        {"leftID": "left2", "rightID": "right1", "priority": 2},
-        {"leftID": "left2", "rightID": "right3", "priority": 3}
-      ]
-    })
-  }
+export default {
+  data: () => ({
+    array: [
+      { leftID: "leftID3", rightID: "rightID", priority: 1 },
+      { leftID: "leftID3", rightID: "rightID", priority: 2 },
+      { leftID: "leftID3", rightID: "rightID2", priority: 3 },
+    ],
+    leftArray: [
+      { leftID: "leftID", label: "boxD" },
+      { leftID: "leftID3", label: "boxC" },
+    ],
+    rightArray: [
+      { rightID: "rightID", label: "boxA" },
+      { rightID: "rightID2", label: "boxB" },
+      { rightID: "rightID3", label: "boxA" },
+    ],
+  }),
+};
 </script>
--- src/components/BoxArray.vue
+++ src/components/BoxArray.vue
@@ -1,127 +1,147 @@
-
-
 <template>
-    <div id="arrayBox"></div>
+  <div id="arrayBox"></div>
 </template>
 
 <script>
 import $ from "jquery";
-import * as joint from "jointjs"
- export default {
-    props: {
-      array:{
-        type: Array,
-        default: []
+import * as joint from "jointjs";
+export default {
+  props: {
+    array: {
+      type: Array,
+      default: [],
+    },
+    leftArray: {
+      type: Array,
+      default: [],
+    },
+    rightArray: {
+      type: Array,
+      default: [],
+    },
+  },
+  mounted() {
+    this.drawBoxes();
+  },
+  created() {
+    window.addEventListener("resize", this.drawBoxes);
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.drawBoxes);
+  },
+  methods: {
+    drawBoxes() {
+      let linkArray = this.array;
+      let maxArrayLength =
+        this.leftArray.length > this.rightArray.length
+          ? this.leftArray.length
+          : this.rightArray.length;
+      let width = 0;
+      if (window.innerWidth > 800) {
+        width = window.innerWidth * 0.4;
+      } else {
+        width = window.innerWidth * 0.8;
       }
-    },
-    mounted() { 
-      this.drawBoxes()
-    },
-    methods:{
-      drawBoxes() {
-        let linkArray = this.array;
-        let leftArray = linkArray.map((item)=>(item.leftID)).filter((v, i, a) => a.indexOf(v) === i)
-        let rightArray = linkArray.map((item)=>(item.rightID)).filter((v, i, a) => a.indexOf(v) === i)
-        let maxArrayLength = leftArray.length > rightArray.length ? leftArray.length : rightArray.length;
-        let width = 0;
-        if(window.innerWidth > 800){
-          width = window.innerWidth * 0.4;
+
+      let boxwidth = width / 5;
+      let boxheight = boxwidth / 3;
+      let height = maxArrayLength * (boxheight + 20);
+      let namespace = joint.shapes;
+
+      let graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+      var paper = new joint.dia.Paper({
+        el: document.getElementById("arrayBox"),
+        width: width,
+        height: height,
+        model: graph,
+        defaultConnectionPoint: { name: "boundary" },
+        defaultConnector: { name: "smooth" },
+        interactive: { linkMove: false },
+        frozen: true,
+      });
+
+      paper.on("element:label:pointerdown", function (_view, evt) {
+        evt.stopPropagation();
+      });
+
+      paper.on("cell:pointerdown blank:pointerdown", function () {
+        if (window.getSelection) {
+          window.getSelection().removeAllRanges();
+        } else if (document.selection) {
+          document.selection.empty();
         }
-        else{
-          width = window.innerWidth * 0.8;
-        }
-        
-        
-        let boxwidth = width / 5;
-        let boxheight = boxwidth / 3;
-        let height = maxArrayLength * (boxheight + 20)
-        let namespace = joint.shapes; 
+      });
 
-        let graph = new joint.dia.Graph({}, { cellNamespace: namespace });
-        var paper = new joint.dia.Paper({
-            el: document.getElementById('arrayBox'),
-            width: width,
-            height: height,
-            model: graph,
-            defaultConnectionPoint: { name: 'boundary' },
-            defaultConnector: { name: 'smooth' },
-            interactive: { linkMove: false },
-            frozen: true,
-        });
-
-        paper.on('element:label:pointerdown', function (_view, evt) {
-          evt.stopPropagation();
-        });
-
-        paper.on('cell:pointerdown blank:pointerdown', function () {
-          if (window.getSelection) {
-            window.getSelection().removeAllRanges();
-          } else if (document.selection) {
-            document.selection.empty();
-          }
-        });
-        
-        function createBox(x, y, label) {
-          var circle = new joint.shapes.standard.Rectangle({
-            position: { x: x, y: y },
-            size: { width: boxwidth, height: boxheight },
-            attrs: {
-              label: {
-                text: label,
-                fontWeight: 'bold',
-                cursor: 'text',
-                style: {
-                  userSelect: 'text',
-                },
-              },
-              body: {
-                strokeWidth: 1,
+      function createBox(x, y, label) {
+        var circle = new joint.shapes.standard.Rectangle({
+          position: { x: x, y: y },
+          size: { width: boxwidth, height: boxheight },
+          attrs: {
+            label: {
+              text: label,
+              fontWeight: "bold",
+              cursor: "text",
+              style: {
+                userSelect: "text",
               },
             },
-          });
-          return circle.addTo(graph);
-        }
-
-        function link(source, target, priority, vertices) {
-          var link = new joint.shapes.standard.Link({
-            source: { id: source.id },
-            target: { id: target.id },
-            attrs: {
-              line: {
-                strokeWidth: priority || 1,
-              },
+            body: {
+              strokeWidth: 1,
             },
-            vertices: vertices || [],
-          });
-          return link.addTo(graph);
-        }
-
-        let leftarraybox = [];
-        let rightArraybox = [];
-        leftArray.map((item, index)=>{
-          let Box = createBox(10, 10 + index * 50, item);
-          leftarraybox.push(Box)
-        })
-        rightArray.map((item, index)=>{
-          let Box = createBox(width - 10 -boxwidth , 10 + index * 50, item);
-          rightArraybox.push(Box)
-        })
-        linkArray.map((item, index)=>{
-          link(leftarraybox[leftArray.indexOf(item.leftID)], rightArraybox[rightArray.indexOf(item.rightID)], item.priority)
-        })
-        paper.unfreeze();
+          },
+        });
+        return circle.addTo(graph);
       }
+
+      function link(source, target, priority, vertices) {
+        var link = new joint.shapes.standard.Link({
+          source: { id: source.id },
+          target: { id: target.id },
+          attrs: {
+            line: {
+              strokeWidth: priority || 1,
+            },
+          },
+          vertices: vertices || [],
+        });
+        return link.addTo(graph);
+      }
+
+      let leftarraybox = [];
+      let rightArraybox = [];
+
+      this.leftArray.map((item, index) => {
+        let Box = createBox(10, 10 + index * 50, item.label);
+        leftarraybox.push(Box);
+      });
+
+      this.rightArray.map((item, index) => {
+        let Box = createBox(width - 10 - boxwidth, 10 + index * 50, item.label);
+        rightArraybox.push(Box);
+      });
+
+      const leftArrayId = this.leftArray
+        .map((item) => item.leftID)
+        .filter((v, i, a) => a.indexOf(v) === i);
+
+      const rightArrayId = this.rightArray
+        .map((item) => item.rightID)
+        .filter((v, i, a) => a.indexOf(v) === i);
+
+      linkArray.map((item) => {
+        link(
+          leftarraybox[leftArrayId.indexOf(item.leftID)],
+          rightArraybox[rightArrayId.indexOf(item.rightID)],
+          item.priority
+        );
+      });
+      paper.unfreeze();
     },
-     created() {
-        window.addEventListener("resize", this.drawBoxes);
-     },
-     destroyed() {
-       window.removeEventListener("resize", this.drawBoxes);
-     }
-  }
+  },
+};
 </script>
 <style>
-#arrayBox{
-  margin:auto
+#arrayBox {
+  margin: auto;
 }
 </style>
